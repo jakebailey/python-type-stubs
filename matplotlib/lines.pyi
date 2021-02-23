2f77a43099bebd0accdb45e3be7ac4c6cfc9b1d2
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2021-02-23 15:50:12 -0800

    Add constructor for Line2D

--- matplotlib/lines.pyi
+++ matplotlib/lines.pyi
@@ -1,32 +1,38 @@
-from typing import Any, Optional
+from matplotlib.artist import Artist
+from typing import Any, List, Literal, Optional, Tuple, Union
 
 from matplotlib._typing import ArrayLike
 from matplotlib.colors import _ColorLike
 from matplotlib.markers import MarkerStyle
 
-class Line2D:
+_LineStyle = Union[
+    Literal['-', 'solid', '--', 'dashed', '-.', 'dashdot', ':', 'dotted', 'None', '  ', ''],
+    Tuple[int, Tuple[int, ...]]
+]
+
+class Line2D(Artist):
     def __init__(
         self,
         xdata: ArrayLike,
         ydata: ArrayLike,
         linewidth: Optional[float] = ...,
-        linestyle=None,
+        linestyle: Optional[_LineStyle] = ...,
         color: Optional[_ColorLike] = ...,
         marker: Optional[MarkerStyle] = ...,
         markersize: Optional[float] = ...,
-        markeredgewidth=None,
+        markeredgewidth: Optional[float] = ...,
         markeredgecolor: Optional[_ColorLike] = ...,
         markerfacecolor: Optional[_ColorLike] = ...,
         markerfacecoloralt: Optional[_ColorLike] = ...,
-        fillstyle=None,
-        antialiased=None,
-        dash_capstyle=None,
-        solid_capstyle=None,
-        dash_joinstyle=None,
-        solid_joinstyle=None,
+        fillstyle: Optional[Literal['full', 'left', 'right', 'bottom', 'top', 'none']] = ...,
+        antialiased: Optional[bool] = ...,
+        dash_capstyle: Optional[Literal['butt', 'round', 'projecting']] = ...,
+        solid_capstyle: Optional[Literal['butt', 'round', 'projecting']] = ...,
+        dash_joinstyle: Optional[Literal['miter', 'round', 'bevel']] = ...,
+        solid_joinstyle: Optional[Literal['miter', 'round', 'bevel']] = ...,
         pickradius: float = ...,
-        drawstyle=None,
-        markevery=None,
+        drawstyle: Optional[Literal['default', 'steps', 'steps-pre', 'steps-mid', 'steps-post']] = ...,
+        markevery: Optional[Union[int, Tuple[int, int], slice, List[int], float, Tuple[float, float], List[bool]]] = ...,
         **kwargs: Any,
     ) -> None: ...
 
